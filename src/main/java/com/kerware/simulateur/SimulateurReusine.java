4c11dd18d6ea71778e9a13ba1f3d44eaf53df81a
BOUIN Yohan <bouin.yohan@outlook.fr>
2026-04-27 22:00:40 +0200

    Ajout de la fonction calculAbatement et calculParts

--- src/main/java/com/kerware/simulateur/SimulateurReusine.java
+++ src/main/java/com/kerware/simulateur/SimulateurReusine.java
@@ -52,7 +52,7 @@ public class SimulateurReusine implements ICalculateurImpot{
     
     //Paramètre Client
     private int revenuNet = 0;
-    private int nbEnfant = 0;
+    private int nbEnfantTotal = 0;
     private int nbEnfantHandicape = 0;
     private boolean isParentIsole = false;
     private SituationFamiliale situationFamiliale = SituationFamiliale.CELIBATAIRE;
@@ -85,7 +85,7 @@ public class SimulateurReusine implements ICalculateurImpot{
 
     @Override
     public void setNbEnfantsACharge(int nbEnfant) {
-    	this.nbEnfant = nbEnfant;
+    	this.nbEnfantTotal = nbEnfant;
     }
 
     @Override
@@ -132,7 +132,191 @@ public class SimulateurReusine implements ICalculateurImpot{
     
     
     //Fonction de calcul
+    /*
     @Override
     public void calculImpotSurRevenuNet() {
+        // Abattement
+
+        abt = rNet * tAbt;
+
+        if (abt > lAbtMax) {
+            abt = lAbtMax;
+        }
+
+        if (abt < lAbtMin) {
+            abt = lAbtMin;
+        }
+
+
+        rFRef = rNet - abt;
+
+        // parts déclarants
+        switch ( sitFam ) {
+            case CELIBATAIRE:
+                nbPtsDecl = 1;
+                break;
+            case MARIE:
+                nbPtsDecl = 2;
+                break;
+            case DIVORCE:
+                nbPtsDecl = 1;
+                break;
+            case VEUF:
+                if ( nbEnf == 0 ) {
+                    nbPtsDecl = 1;
+                } else {
+                    nbPtsDecl = 2;
+                }
+                nbPtsDecl = 1;
+                break;
+        }
+
+        // parts enfants à charge
+        if ( nbEnf <= 2 ) {
+            nbPts = nbPtsDecl + nbEnf * 0.5;
+        } else if ( nbEnf > 2 ) {
+            nbPts = nbPtsDecl+  1.0 + ( nbEnf - 2 );
+        }
+
+        // parent isolé
+        if ( parIso ) {
+            if ( nbEnf > 0 ){
+                nbPts = nbPts + 0.5;
+            }
+        }
+
+        // enfant handicapé
+        nbPts = nbPts + nbEnfH * 0.5;
+
+        // impôt des declarants
+        rImposable = rFRef / nbPtsDecl ;
+
+        mImpDecl = 0;
+
+        int i = 0;
+        do {
+            if ( rImposable >= limites[i] && rImposable < limites[i+1] ) {
+                mImpDecl += ( rImposable - limites[i] ) * taux[i];
+                break;
+            } else {
+                mImpDecl += ( limites[i+1] - limites[i] ) * taux[i];
+            }
+            i++;
+        } while( i < 5);
+
+        mImpDecl = mImpDecl * nbPtsDecl;
+        mImpDecl = Math.round( mImpDecl );
+
+        // impôt foyer fiscal complet
+        rImposable =  rFRef / nbPts;
+        mImp = 0;
+        i = 0;
+
+        do {
+            if ( rImposable >= limites[i] && rImposable < limites[i+1] ) {
+                mImp += ( rImposable - limites[i] ) * taux[i];
+                break;
+            } else {
+                mImp += ( limites[i+1] - limites[i] ) * taux[i];
+            }
+            i++;
+        } while( i < 5);
+
+        mImp = mImp * nbPts;
+        mImp = Math.round( mImp );
+
+        // baisse impot
+        double baisseImpot = mImpDecl - mImp;
+
+        // dépassement plafond
+        double ecartPts = nbPts - nbPtsDecl;
+
+        double plafond = (ecartPts / 0.5) * plafDemiPart;
+
+        if ( baisseImpot >= plafond ) {
+            mImp = mImpDecl - plafond;
+        }
+
+        decote = 0;
+        // decote
+        if ( nbPtsDecl == 1 ) {
+            if ( mImp < seuilDecoteDeclarantSeul ) {
+                 decote = decoteMaxDeclarantSeul - ( mImp  * tauxDecote );
+            }
+        }
+        if (  nbPtsDecl == 2 ) {
+            if ( mImp < seuilDecoteDeclarantCouple ) {
+                 decote =  decoteMaxDeclarantCouple - ( mImp  * tauxDecote  );
+            }
+        }
+        decote = Math.round( decote );
+        if ( mImp <= decote ) {
+            decote = mImp;
+        }
+
+        mImp = Math.round(mImp) - decote;
+
+        return (long) mImp;
     }
+    */
+    
+    @Override
+    public void calculImpotSurRevenuNet() {
+    	calculAbatement();
+    	calculParts();
+    	
+    }
+    
+    private void calculAbatement() {
+        abatement = revenuNet * TAUX_ABATEMENT;
+
+        if (abatement > PALIER_MAXIMUM_POUR_ABATEMENT) {
+            abatement = PALIER_MAXIMUM_POUR_ABATEMENT;
+        }
+
+        if (abatement < PALIER_MINIMUM_POUR_ABATEMENT) {
+            abatement = PALIER_MINIMUM_POUR_ABATEMENT;
+        }
+
+        revenuFiscalReference = revenuNet - abatement;
+    }
+    
+    private void calculParts() {
+    	//µPartie des déclarants
+        switch ( situationFamiliale ) {
+            case CELIBATAIRE:
+            	nbPartsDeclarant = 1; break;
+            case MARIE:
+            	nbPartsDeclarant = 2; break;
+            case DIVORCE:
+            	nbPartsDeclarant = 1; break;
+            case VEUF:
+            	/*
+                if ( nbEnfantTotal == 0 ) {
+                	nbPartsDeclarant = 1;
+                } else {
+                	nbPartsDeclarant = 2;
+                }
+                */
+            	nbPartsDeclarant = 1; //Dans le code originel le if else est suivi de cette ligne le rendant inutile, je laisse le code car je ne sais pas si il fallait corrigée sachant que cela pour casser les test.
+                break;
+        }
+        
+
+        //Compte de base pour les enfants
+        if ( nbEnfantTotal <= 2 ) {
+        	nbParts = nbPartsDeclarant + nbEnfantTotal * 0.5;
+        } else if ( nbEnfantTotal > 2 ) { //Les part changes s'il y a plus de 2 enfants
+        	nbParts = nbPartsDeclarant +  1.0 + ( nbEnfantTotal - 2 );
+        }
+
+        //Ajout des part suplémentaire en cas de parent isolé avec enfant
+        if ( isParentIsole && nbEnfantTotal > 0) {
+            nbParts = nbParts + 0.5;
+        }
+
+        //Ajout des part suplémentaire pour chaque cas d'enfant handicapé
+        nbParts = nbParts + nbEnfantHandicape * 0.5;
+    }
+    
 }
